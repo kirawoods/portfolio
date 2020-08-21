67a1633fd5bd7c8c0a41c5a1d6e8423c2e9298b1
Kira <kawoods826@gmail.com>
2020-08-21 10:08:14 -0700

    updates styling

--- src/components/homepage/PortfolioSection.js
+++ src/components/homepage/PortfolioSection.js
@@ -1,6 +1,22 @@
 import React from "react";
 import "./PortfolioSection.css";
+import { Link } from "react-router-dom";
 
 export function PortfolioSection() {
-  return <div className="PortfolioSection">Portfolio Section</div>;
+  return (
+    <div className="PortfolioSection">
+      <div className="portfolio-header">PORTFOLIO</div>
+      <div className="horizontal-line"></div>
+      <div className="featured-project"></div>
+      <div className="other-projects-container">
+        <div className="project"></div>
+        <div className="project"></div>
+        <div className="project"></div>
+      </div>
+
+      <Link className="portfolio-button" to="/portfolio">
+        MORE PROJECTS
+      </Link>
+    </div>
+  );
 }
