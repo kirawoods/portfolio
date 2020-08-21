df4376c401c41e4c4e89310ea395bd2f692dc3af
Kira <kawoods826@gmail.com>
2020-08-21 10:27:29 -0700

    updates styling

--- src/components/homepage/ContactSection.js
+++ src/components/homepage/ContactSection.js
@@ -4,8 +4,17 @@ import "./ContactSection.css";
 export function ContactSection() {
   return (
     <div className="ContactSection">
-      <div className="contact-header">GET IN TOUCH</div>
-      <div className="horizontal-line"></div>
+      <div className="contact-section-text-container">
+        <div className="contact-header">GET IN TOUCH</div>
+        <div className="horizontal-line"></div>
+        <div className="contact-subheading">
+          I'm currently available for freelance work and full-time roles.
+        </div>
+        <div className="contact-section-text">
+          If you have a project you want to get started, think I'd be a good fit
+          for your engineering team, or just want to say hey, then get in touch.
+        </div>
+      </div>
     </div>
   );
 }
