a05e87f535036bde88b04ebd079290a30700dbc9
Kira <kawoods826@gmail.com>
2020-08-25 21:31:04 -0700

    updates styling

--- src/components/homepage/PortfolioSection.js
+++ src/components/homepage/PortfolioSection.js
@@ -10,24 +10,31 @@ export function PortfolioSection() {
     <div className="PortfolioSection">
       <div className="portfolio-header">PORTFOLIO</div>
       <div className="horizontal-line"></div>
-      <div className="featured-project">
-        <img src={sfrestaurants} />
-      </div>
+      <a href="https://sfrestaurants.netlify.app/" target="blank">
+        <div className="featured-project">
+          <img src={sfrestaurants} />
+        </div>
+      </a>
       <div className="other-projects-container">
         <div className="project mastermind-container">
-          <img className="mastermind-logo" src={mastermind}></img>
+          <a href="https://mastermindkw.netlify.app/" target="blank">
+            <img className="mastermind-logo" src={mastermind} />
+          </a>
         </div>
+
         <div className="project advo-container">
           <div className="advo-text">Advo</div>
           <div className="advo-description">
             Advo empowers young adults to take charge of their mental health by
-            using their own data to make better decisions. It's currently in
+            using their own data to improve their wellbeing. It's currently in
             stealth mode so I won't publicly share the code. If you're
             interested in learning more, please contact me.
           </div>
         </div>
         <div className="project">
-          <img className="helper-helper-logo" src={helperhelper} />
+          <a href="https://helperhelper.netlify.app/" target="blank">
+            <img className="helper-helper-logo" src={helperhelper} />
+          </a>
         </div>
       </div>
 
