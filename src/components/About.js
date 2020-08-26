e8e3e682bb160670934342da10e43546de93a783
Kira <kawoods826@gmail.com>
2020-08-25 22:17:40 -0700

    updates styling

--- src/components/About.js
+++ src/components/About.js
@@ -4,65 +4,68 @@ import "./About.css";
 export function About() {
   return (
     <div className="About">
-      <div className="timeline-text-container">
-        <div className="timeline-section">
-          March 2020: Received an offer for an engineering role at LinkedIn the
-          day before they announced their hiring freeze. Better luck next time.
-        </div>
-        <div className="timeline-section">
-          July 2019: Began attending a coding bootcamp to brush up on my coding
-          skills. I left after realizing that I already knew more than I
-          realized and figured I'd learn even more working on the startup. I
-          created a framework for my own learning curriculum, recruited some
-          mentors, and never looked back.
-        </div>
-        <div className="timeline-section">
-          June 2019: Co-founded a start-up called Happy at Home with Rich Pappas
-          and Hal Woods, and began work on our flagship product, Advo.
-        </div>
-        <div className="timeline-section">
-          January 2019: Earned a master's in occupational therapy and began
-          working as a pediatric OT. In addition to my work as a therapist, I
-          act as tech support for my co-workers. I also built our company
-          website and I'm working on building an online tool to streamline our
-          evaluation process.
-        </div>
-        <div className="timeline-section">
-          August 2015: Moved to the SF Bay Area.
-        </div>
-        <div className="timeline-section">
-          May 2015: Graduated from Pomona College with a degree in International
-          Relations. I wrote my thesis on strategies to combat human
-          trafficking. This began my dreams of createing technological solutions
-          to solve human problems. I was torn between going into a healthcare
-          role where I could help people directly or continuing to pursue and
-          interest in technology. After much deliberation I decided that I
-          wanted to do both, but it made sense to pursue formal education in the
-          field of Occupational Therapy. It turns out that you can build
-          software without being a licensed practitioner, but not so much for
-          healthcare.
-        </div>
-        <div className="timeline-section">
-          January 2013: I founded my first company, Craft Change. It still
-          exists today as an outlet for my art and a way to support causes I
-          care about. This company was also the beginning of my long-term love
-          of programming. I first started styling divs building the Craft Change
-          website in Wordpress.
-        </div>
-        <div className="timeline-section">
-          May 2011: I graduated high school as valedictorian with the school
-          science award in tow after being told for four years by my teachers
-          that girls can't do math or science. I've outgrown the chip on my
-          shoulder, but I do hope to one day return home to start a program for
-          middle and high school girls who are interested in STEM. I don't think
-          it would've taken me so long to find software engineering if I'd had
-          the same opportunity.
-        </div>
-        <div className="timeline-section">
-          August 1993: Born to two lovely parents in Colorado Springs, CO.
+      <div className="timeline-container">
+        <div className="vertical-line"></div>
+        <div className="timeline-text-container">
+          <div className="timeline-section">
+            March 2020: Received an offer for an engineering role at LinkedIn
+            the day before they announced their hiring freeze. Better luck next
+            time.
+          </div>
+          <div className="timeline-section">
+            July 2019: Began attending a coding bootcamp to brush up on my
+            coding skills. I left after realizing that I already knew more than
+            I realized and figured I'd learn even more working on the startup. I
+            created a framework for my own learning curriculum, recruited some
+            mentors, and never looked back.
+          </div>
+          <div className="timeline-section">
+            June 2019: Co-founded a start-up called Happy at Home with Rich
+            Pappas and Hal Woods, and began work on our flagship product, Advo.
+          </div>
+          <div className="timeline-section">
+            January 2019: Earned a master's in occupational therapy and began
+            working as a pediatric OT. In addition to my work as a therapist, I
+            act as tech support for my co-workers. I also built our company
+            website and I'm working on building an online tool to streamline our
+            evaluation process.
+          </div>
+          <div className="timeline-section">
+            August 2015: Moved to the SF Bay Area.
+          </div>
+          <div className="timeline-section">
+            May 2015: Graduated from Pomona College with a degree in
+            International Relations. I wrote my thesis on strategies to combat
+            human trafficking. This began my dreams of createing technological
+            solutions to solve human problems. I was torn between going into a
+            healthcare role where I could help people directly or continuing to
+            pursue and interest in technology. After much deliberation I decided
+            that I wanted to do both, but it made sense to pursue formal
+            education in the field of Occupational Therapy. It turns out that
+            you can build software without being a licensed practitioner, but
+            not so much for healthcare.
+          </div>
+          <div className="timeline-section">
+            January 2013: I founded my first company, Craft Change. It still
+            exists today as an outlet for my art and a way to support causes I
+            care about. This company was also the beginning of my long-term love
+            of programming. I first started styling divs building the Craft
+            Change website in Wordpress.
+          </div>
+          <div className="timeline-section">
+            May 2011: I graduated high school as valedictorian with the school
+            science award in tow after being told for four years by my teachers
+            that girls can't do math or science. I've outgrown the chip on my
+            shoulder, but I do hope to one day return home to start a program
+            for middle and high school girls who are interested in STEM. I don't
+            think it would've taken me so long to find software engineering if
+            I'd had the same opportunity.
+          </div>
+          <div className="timeline-section">
+            August 1993: Born to two lovely parents in Colorado Springs, CO.
+          </div>
         </div>
       </div>
-      <div className="vertical-line"></div>
     </div>
   );
 }
